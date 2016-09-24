fc84a98f4e8f04630bb618007de5e7a2feba37ac
Jaroslaw Zabiello <hipertracker@gmail.com>
2016-09-25 00:41:56 +0100

    add number of completed todos

--- src/components/App/Controls/ClearCompleted/index.js
+++ src/components/App/Controls/ClearCompleted/index.js
@@ -11,7 +11,7 @@ export default connect({}, {
         hidden={entriesCompleted === 0}
         onClick={() => clicked()}
       >
-        Clear completed
+        Clear completed ({entriesCompleted})
       </button>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import 'todomvc-common/base.css'
 import 'todomvc-app-css/index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {render} from 'react-dom'
 import {Container} from 'cerebral-view-react'
 import controller from './controller'
 
 import App from './components/App'
 
-ReactDOM.render(
+render(
   <Container controller={controller} strict>
     <App/>
-  </Container>, document.querySelector('#root'))
+  </Container>, document.querySelector('#app'))
